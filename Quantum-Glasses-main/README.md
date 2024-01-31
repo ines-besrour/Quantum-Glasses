df0e14704d4c06c7908b25632c5cf338ea564821
ines-besrour <inesbes2001@gmail.com>
2024-01-31 16:22:08 +0100

    quantum glasses

---
+++ Quantum-Glasses-main/README.md
@@ -0,0 +1,15 @@
+# Quantum-Glasses
+
+### Launch
+
+To launch the app just run :
+```bash
+python quantum_glasses.py
+```
+
+To launch unittests just run :
+```bash
+tox -epy39
+```
+
+Quantum Computing based Tkinter GUI App, using qiskit, QuantumCircuit, This App is a visualization tool It helps to visualize single-qubit rotations on Bloch Sphere.
---
+++ tests/test_quantum_glasses.py
@@ -0,0 +1,16 @@
+from quantum_glasses import QuantumGlasses
+import unittest
+import os
+
+class TestQuantumGlasses(unittest.TestCase):
+
+    def test_main(self):
+        """Global test."""
+        my_qglases = QuantumGlasses()
+
+        if os.getenv('DISPLAY') is None:
+            os.environ.__setitem__('DISPLAY', ':0.0')
+            with self.assertRaises(Exception):
+                my_qglases.main(testing=True)
+        else:
+            self.assertEqual(my_qglases.main(testing=True), True)
